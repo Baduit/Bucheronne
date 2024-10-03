e7abc24afe391e15e53eff7c3c9bbce536098951
Lena Bertho <23459846+Baduit@users.noreply.github.com>
2024-10-04 00:18:37 +0200

    ✨Add command to merge PRs using branches name

--- src/bucheronne/__init__.py
+++ src/bucheronne/__init__.py
@@ -1,7 +1,7 @@
 import click
 from github import Github, Consts
 
-from .github_branch import create_branch, create_new_pr, delete_branch
+from .github_branch import create_branch, create_new_pr, delete_branch, merge_pr_by_branch_names
 from .github_token import deduce_token, read_from_file
 
 
@@ -45,12 +45,28 @@ def delete(repos, hostname, token_path, branch) -> int:
 @click.option("--hostname", "-h", type=str, help="Hostname, useful for github enterprise with custom hostname")
 @click.option("--token-path", "-t", type=str, help="Path of the file where the token is stored")
 @click.option("--head", "-h", required=True, type=str, help="Name of the branch you want to merge")
-@click.option("--base", "-b", type=str, default='main', help="Name of the branch you want to merge into")
-@click.option("--title", "-i", type=str, default='main', help="Title of the PRs.")
+@click.option("--base", "-b", required=True, type=str, default='main', help="Name of the branch you want to merge into")
+@click.option("--title", "-i", required=True, type=str, default='main', help="Title of the PRs.")
 def create_pr(repos, hostname, token_path, head, base, title) -> int:
     auth = read_from_file(token_path) if token_path else deduce_token()
     base_url = f"https://{hostname}/api/v3" if hostname else Consts.DEFAULT_BASE_URL
     with Github(auth=auth, base_url=base_url) as g:
         for repo in repos:
             create_new_pr(g, repo, head, base, title)
+    return 0
+
+
+@main.command()
+@click.argument('repos', nargs=-1)
+@click.option("--hostname", "-h", type=str, help="Hostname, useful for github enterprise with custom hostname")
+@click.option("--token-path", "-t", type=str, help="Path of the file where the token is stored")
+@click.option("--head", "-h", required=True, type=str, help="Name of the branch you want to merge")
+@click.option("--base", "-b", required=True, type=str, default='main', help="Name of the branch you want to merge into")
+@click.option("--delete-branch", "-d", type=bool, default=True, help="Flag to delete or not the branch afterward")
+def merge_pr(repos, hostname, token_path, head, base, delete_branch) -> int:
+    auth = read_from_file(token_path) if token_path else deduce_token()
+    base_url = f"https://{hostname}/api/v3" if hostname else Consts.DEFAULT_BASE_URL
+    with Github(auth=auth, base_url=base_url) as g:
+        for repo in repos:
+            merge_pr_by_branch_names(g, repo, head, base, "rebase", delete_branch)
     return 0
--- src/bucheronne/github_branch.py
+++ src/bucheronne/github_branch.py
@@ -26,3 +26,11 @@ def create_new_pr(g: Github, repo_name: str, head_branch: str, base_branch: str,
 	repo = g.get_repo(repo_name)
 	pr = repo.create_pull(base=base_branch, head=head_branch, title=title)
 	print(f"Pull Request created successfully: {pr.html_url}")
+
+
+def merge_pr_by_branch_names(g: Github, repo_name: str, head_branch: str, base_branch: str, merge_method: str, delete_branch: bool):
+	repo = g.get_repo(repo_name)
+	pull_requests = repo.get_pulls(base=base_branch, head=head_branch, state="open")
+	assert pull_requests.totalCount == 1
+	pull_requests[0].merge(merge_method=merge_method, delete_branch=delete_branch)
+	print("Sucess")
